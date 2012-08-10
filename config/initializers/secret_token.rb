21796d5a0df68bb6cdf493819aa40bb9ef5a8595
Erlendur Eiriksson <erlendure@gmail.com>
2012-08-10 09:26:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteOffers::Application.config.secret_token = '560ccb90174694c93f4fd139e4d849980732ed86d20e7bd4865caea1ac3075eba4e3d58455e563d0e417356d28879ae1d861bd19a0cc4a711ad31557e8c8ad4d'
